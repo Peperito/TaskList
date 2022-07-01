5876f824a8e5d0402516bc9f0afcd237f9d6c6aa
Vince P <32515747+Peperito@users.noreply.github.com>
2022-07-01 15:43:38 +0200

    basic tasklist no css

---
+++ tasklist/src/AppFunction.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import NewTask from "./NewTask";
+import TasksList from "./TasksList";
+
+export default function AppFunction() {
+  const [newTask, setNewTask] = useState({});
+  const [allTasks, setAllTasks] = useState([]);
+
+  const handleChange = ({target}) => {
+    const {name, value} = target;
+    setNewTask((prev) => ({
+      ...prev,
+      [name]: value,
+      id: Date.now()
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!newTask.title) return;
+    setAllTasks((prev)=>[newTask, ...prev]);
+    setNewTask({});
+   };
+
+ const handleDelete = (taskIdToRemove) => {
+    setAllTasks((prev) => prev.filter(
+      (task) => task.id !== taskIdToRemove
+    ));
+  };
+    return (
+      <main>
+        <h1>Tasks</h1>
+        <NewTask
+        newTask={newTask}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        />
+
+        <TasksList
+          allTasks={allTasks}
+          handleDelete={handleDelete}
+        />
+      </main>
+    );
+
+  }
--- tasklist/src/index.js
+++ tasklist/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AppFunction from './AppFunction';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <AppFunction />,
   document.getElementById('root')
 );
